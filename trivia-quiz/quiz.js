449658a1c9c3d62e283704802a43c9928fc9e784
DougKF <douglaskforbes@gmail.com>
2023-09-18 16:53:47 +0100

    Adding some comments in trivia quiz

--- trivia-quiz/quiz.js
+++ trivia-quiz/quiz.js
@@ -1,13 +1,27 @@
 import { questions } from "./questions.js";
 
-function createTextToDisplayQuestion(questionObject) {
-  const choices = Object.entries(questionObject.choices)
-  .map(([letter, choice]) => `${letter}) ${choice}`)
-  .join("\n");
+// ********************
+// * define functions *
+// ********************
 
+// format the question for display and return the text
+function createTextToDisplayQuestion(questionObject) {
+  // string interpolation / string templates
+  // define a string between ``
+  // include any variables in that string by putting ${variable name}
+
+  // uses arrow notation to define function for map
+  // essentally takes letter and choice out of the input object
+  // and gives back a new array entry
+
+  // don't worry if you don't follow this bit of code - it is just
+  // formatting some text
+  const choices = Object.entries(questionObject.choices)
+    .map(([letter, choice]) => `${letter}) ${choice}`)
+    .join("\n");
 
   const textToDisplay = `#${questionObject.number} ${questionObject.question}\n\n${choices}\n\nPlease enter either A, B, C or D.\n`;
-  console.log(textToDisplay)
+  console.log(textToDisplay);
   return textToDisplay;
 }
 
@@ -15,7 +29,12 @@ function getSelectedChoiceFromUser(questionText) {
   while (true) {
     const rawChoice = prompt(questionText);
     const standardised = rawChoice?.toUpperCase();
-    if ("A" === standardised || "B" === standardised || "C" === standardised || "D" === standardised) {
+    if (
+      "A" === standardised ||
+      "B" === standardised ||
+      "C" === standardised ||
+      "D" === standardised
+    ) {
       return standardised;
     } else if (undefined === standardised) {
       return null;
@@ -25,17 +44,22 @@ function getSelectedChoiceFromUser(questionText) {
 }
 
 function playGame() {
-  const userHasCancelled = !confirm("The quiz is about to begin. Are you sure you want to play?");
+  // ask user if they want to continuequit
+  const userHasCancelled = !confirm(
+    "The quiz is about to begin. Are you sure you want to play?"
+  );
+  // check user response
   if (userHasCancelled) {
     return alert("You've cancelled the quiz, no more questions will be shown.");
   }
 
   let score = 0;
 
+  // for each question from the questions array (defined in questions.js)
   for (let i = 0; i < questions.length; i++) {
-    // this question is an object
+    // get the question object from the questions array
     const question = questions[i];
-    console.log(question)
+    console.log(question);
 
     // we're plugging the question object in the function to format it correctly
     const text = createTextToDisplayQuestion(question);
@@ -43,19 +67,29 @@ function playGame() {
 
     const userHasCancelled = null === userChoice;
     if (userHasCancelled) {
-      return alert("You've cancelled the quiz, no more questions will be shown.");
+      return alert(
+        "You've cancelled the quiz, no more questions will be shown."
+      );
     }
 
     const userHasAnsweredIncorrectly = userChoice !== question.correctChoice;
     if (userHasAnsweredIncorrectly) {
-      return alert(`Incorrect, you lose! The correct answer was ${question.correctChoice}. Your score was ${score}.`);
+      return alert(
+        `Incorrect, you lose! The correct answer was ${question.correctChoice}. Your score was ${score}.`
+      );
     }
 
     score++;
     alert(`Correct! Your score is now ${score}!`);
   }
 
-    alert (`You've reached the end of the quiz, your score was ${score}. Please play again!`);
+  alert(
+    `You've reached the end of the quiz, your score was ${score}. Please play again!`
+  );
 }
+
+// ***********************
+// * program starts here *
+// ***********************
 
 playGame();
